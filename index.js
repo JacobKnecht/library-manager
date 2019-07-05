6d5f8a6a80423bc32e5ec05e0369704828206196
Jacob Knecht <jacobknecht3@gmail.com>
2019-07-05 15:58:29 -0500

    added 'create new book' route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //reqire statements
 const express = require('express');
 const path = require('path');
+const Book = require('../models').Book;
 
 //application variables
 const app = express();
@@ -15,14 +16,18 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 //Home route
 app.get('/', (req, res) => res.redirect('/books'));
 
-//'/books' route shows the full list of books GET
-app.get('/books', (req, res) => {});
+//'/books' route shows the full list of books GET Book.findAll()
+app.get('/books', (req, res) => {}); 
 
 //'/books/new' route shows the 'create new book' form GET
-app.get('/books/new', (req, res) => {});
+app.get('/books/new', (req, res) => {
+  res.render('new-book', {book: Book.build(), title: 'New Book'});
+});
 
-//'/books/new' route posts a new book to the database POST
-app.post('/books/new', (req, res) => {});
+//'/books/new' route posts a new book to the database POST Book.create(req.body)
+app.post('/books/new', (req, res) => {
+
+});
 
 //'/books/:id' route shows 'book detail' form GET
 app.get('/books/:id', (req, res) => {});
